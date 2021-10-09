9d87218ac963c8afa3c1220055af039d00129689
Lauzaki <L@L.local>
2021-10-09 17:40:52 +0100

    rename js to ts

---
+++ lambdas/handlers/get-presigned-url.ts
@@ -0,0 +1,18 @@
+var AWS = require('aws-sdk');
+var s3 = new AWS.S3({
+  signatureVersion: 'v4',
+});
+
+exports.handler = async function(event) {
+  const url = s3.getSignedUrl('putObject', {
+    Bucket: 'cdkworkshopstack-bucket83908e77-1ojw9gyxpwhp2',
+    Key: 'UploadedFile',
+    Expires: 600,
+  });
+
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "text/plain" },
+    body: url
+  };
+};
